cfc5469a60257de4fb2b1a2eb9d718f3cc2f3291
Valentyn Saik <svbackend22@gmail.com>
2020-05-22 10:41:00 +0200

    Flysystem config

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use League\Flysystem\AdapterInterface;
+use League\Flysystem\FilesystemInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +14,24 @@ class UploadController extends BaseController
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(FilesystemInterface $awsStorage): Response
     {
+        $result = $awsStorage->write('hello.txt', 'Hello World!', [
+            'visibility' => AdapterInterface::VISIBILITY_PUBLIC
+        ]);
+
         return $this->render('home/index.html.twig');
+    }
+
+    public function upload(Request $request, FilesystemInterface $awsStorage)
+    {
+        /** @var UploadedFile $file */
+        $file = $request->files->get('photo');
+
+        if ($file->isValid()) {
+            $stream = fopen($file->getRealPath(), 'r+');
+            $awsStorage->writeStream('uploads/'.$file->getClientOriginalName(), $stream);
+            fclose($stream);
+        }
     }
 }
