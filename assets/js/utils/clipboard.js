e87be4fb6d6f8f04eb31c78c4c57505ce061274d
Valentyn Saik <svbackend22@gmail.com>
2020-06-21 09:43:04 +0200

    Copy to clipboard small upd

--- assets/js/utils/clipboard.js
+++ assets/js/utils/clipboard.js
@@ -1,18 +1,10 @@
-export const copyToClipboard = function(text, isModal = null){
-
-    // Copies a string to the clipboard. Must be called from within an
-    // event handler such as click. May return false if it failed, but
-    // this is not always possible. Browser support for Chrome 43+,
-    // Firefox 42+, Safari 10+, Edge and Internet Explorer 10+.
-    // Internet Explorer: The clipboard feature may be disabled by
-    // an administrator. By default a prompt is shown the first
-    // time the clipboard is used (per session).
+export const copyToClipboard = (text) => {
     if (window.clipboardData && window.clipboardData.setData) {
         // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
-        return clipboardData.setData("Text", text);
+        return window.clipboardData.setData("Text", text);
 
     } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-        var textarea = document.createElement("textarea");
+        const textarea = document.createElement("textarea");
         textarea.textContent = text;
         textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
         document.body.appendChild(textarea);
